aaaf0b6852e79b72ecdccc220b02eaddc3b766ef
anantsb <anantbhagat44@gmail.com>
2023-03-11 08:07:45 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import { ThemeProvider } from 'next-themes';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider enableSystem={true} attribute="className">
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
